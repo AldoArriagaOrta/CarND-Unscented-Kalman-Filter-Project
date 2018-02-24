5d09d9e81aa9dc03d9868899d6f360967501ad3e
Aldo Arriaga <aldo.orta@eu4m.eu>
2018-02-24 13:54:40 +0100

    Target RMSE values achieved

--- src/ukf.h
+++ src/ukf.h
@@ -12,9 +12,13 @@ using Eigen::VectorXd;
 
 class UKF {
 public:
+	
 
   ///* initially set to false, set to true in first call of ProcessMeasurement
   bool is_initialized_;
+
+  ///* previous timestamp
+  long long previous_timestamp_;
 
   ///* if this is false, laser measurements will be ignored (except for init)
   bool use_laser_;
@@ -25,8 +29,29 @@ public:
   ///* state vector: [pos1 pos2 vel_abs yaw_angle yaw_rate] in SI units and rad
   VectorXd x_;
 
+  ///* augmented state vector : 
+  VectorXd x_aug_;
+
   ///* state covariance matrix
   MatrixXd P_;
+
+  ///* augmented state covariance matrix
+  MatrixXd P_aug_;
+  
+  ///* 
+  MatrixXd Q_;
+
+  ///* lidar measurement noise matrix
+  MatrixXd R_lidar_;
+
+  ///* radar measurement noise matrix
+  MatrixXd R_radar_;
+
+  ///* sigma points matrix
+  MatrixXd Xsig_;
+
+  ///* augmented sigma points matrix
+  MatrixXd Xsig_aug_;
 
   ///* predicted sigma points matrix
   MatrixXd Xsig_pred_;
@@ -55,6 +80,12 @@ public:
   ///* Radar measurement noise standard deviation radius change in m/s
   double std_radrd_ ;
 
+  ///* squared root of state dimension times lambda 
+  double nlsq_;
+
+  ///* Coefficient for sigma point generation : sqrt( lamba + n_x) -->or n_aug
+  double nlsq_aug_;
+
   ///* Weights of sigma points
   VectorXd weights_;
 
@@ -64,8 +95,20 @@ public:
   ///* Augmented state dimension
   int n_aug_;
 
+  ///* Lidar measurement dimension
+  int n_zl_;
+  
+  ///* Radar measurement dimension
+  int n_zr_;
+
   ///* Sigma point spreading parameter
   double lambda_;
+
+  ///* NIS radar
+  double NIS_radar_;
+
+  ///* NIS lidar
+  double NIS_lidar_;
 
 
   /**
@@ -92,16 +135,10 @@ public:
   void Prediction(double delta_t);
 
   /**
-   * Updates the state and the state covariance matrix using a laser measurement
-   * @param meas_package The measurement at k+1
-   */
-  void UpdateLidar(MeasurementPackage meas_package);
-
-  /**
-   * Updates the state and the state covariance matrix using a radar measurement
-   * @param meas_package The measurement at k+1
-   */
-  void UpdateRadar(MeasurementPackage meas_package);
+  * Updates the state and the state covariance matrix 
+  * @param meas_package The measurement at k+1
+  */
+  void Update(MeasurementPackage meas_package);
 };
 
 #endif /* UKF_H */
